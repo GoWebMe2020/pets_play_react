7c8c5ab5f07c324b9a2070a15c13782906ce4f62
JP Ferreira <jp.ferreira@bridge-u.com>
2022-10-15 15:39:43 +0100

    Refactoring colors and form states

--- src/Components/Forms/UserForm.js
+++ src/Components/Forms/UserForm.js
@@ -8,23 +8,10 @@ import RegistrationForm from './RegistrationForm';
 
 function UserForm() {
 
-  let [formType, setFormType] = useState("login")
-  let [formAction, setFormAction] = useState("Sign in")
-  let [formOption, setFormOption] = useState("Sign up")
-  let [formStatus, setFormStatus] = useState("Not a member?")
+  const [loginForm, setLoginForm] = useState(true)
 
   const changeFormHandler = () => {
-    if (formType === "login") {
-      formType = setFormType("register")
-      formAction = setFormAction("Sign up")
-      formOption = setFormOption("Sign in")
-      formStatus = setFormStatus("Already a member?")
-    } else {
-      formType = setFormType("login")
-      formAction = setFormAction("Sign in")
-      formOption = setFormOption("Sign up")
-      formStatus = setFormStatus("Not a member?")
-    }
+    setLoginForm(!loginForm)
   }
 
   return (
@@ -37,18 +24,14 @@ function UserForm() {
           <FunIcon />
           <h1 className="ms-2 mb-0">PetsPlay</h1>
         </div>
-        <h2>{formAction}</h2>
+        <h2>{loginForm ? "Sign in" : "Sign up"}</h2>
         <p>Let your pet live its best life</p>
 
         <HorizontalLine lineColor="dark-green"/>
 
-        {formType === "login" ? (
-          <LoginForm />
-        ) : (
-          <RegistrationForm />
-        )}
+        {loginForm ? (<LoginForm />) : (<RegistrationForm />)}
 
-        <p className="mt-3 mb-0">{formStatus} <span className="text-click" onClick={changeFormHandler}>{formOption} now</span></p>
+        <p className="mt-3 mb-0">{loginForm ? "Not a member?" : "Already a member?"} <span className="text-click" onClick={changeFormHandler}>{loginForm ? "Sign up" : "Sign in"} now</span></p>
       </div>
     </div>
   )
